df4af76dbdc5c4af11c50f9d6e0ddfc17f8b8824
julian.arboleda <julian2320@gmail.com>
2023-11-08 12:44:54 -0500

    agregado modelos

--- sistema_odonto/control_odonto/models.py
+++ sistema_odonto/control_odonto/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+class Pacientes(models.Model):
+# los atributos de clase (son las columnas de la tabla)
+    nombre = models.CharField(max_length=64)
+    apellido = models.CharField(max_length=256)
+    genero = models.CharField(max_length=256)
+    identificacion  = models.CharField(max_length=32)
+    antecedentes = models.TextField(max_length=500)
+    tipo_id = models.CharField(max_length=256)
+    fecha_nacimiento = models.DateField(null=True)
+    email = models.EmailField(blank=True)
+     
+
+class Odontologo(models.Model):
+    nombre = models.CharField(max_length=256)
+    apellido = models.CharField(max_length=256)
+    email = models.EmailField(blank=True)
+    telefono = models.CharField(max_length=20, blank=True)
+    identidicacion = models.CharField(max_length=32)
+    fecha_nacimiento = models.DateField(null=True)
+    especialidad = models.CharField(max_length=32)
+
+class Consultas (models.Model):
+    profesional = models.CharField(max_length=256)
+    fecha_consulta =  models.DateField(null=True)
+    identificacion = models.CharField(max_length=32)
+    codigo_cups = models.IntegerField(max_length=5000)
+    finalidad_consulta = models.DateField(null=True, blank=True)
+    causa_externa = models.IntegerField(max_length=5000)
+    codigo_diagnostico_ppal = models.IntegerField(max_length=5000)
+    codigo_diag_opc = models.IntegerField(max_length=5000)
+    valor_de_consulta = models.IntegerField(max_length=5000)
+    valor_total = models.IntegerField(max_length=5000)
+    
+
+class Empleados (models.Model):
+    nombre = models.CharField(max_length=64)
+    apellido = models.CharField(max_length=256)
+    genero = models.CharField(max_length=256)
+    identificacion  = models.CharField(max_length=32)
+    antecedentes = models.TextField(max_length=500)
+    tipo_id = models.CharField(max_length=256)
+    fecha_nacimiento = models.DateField(null=True)
+    email = models.EmailField(blank=True)
