cee90c0212fc6c0f7fbeda1f5144b6e4d87dbd87
julian.arboleda <julian2320@gmail.com>
2023-11-09 14:03:42 -0500

    varios

--- sistema_odonto/control_odonto/urls.py
+++ sistema_odonto/control_odonto/urls.py
@@ -1,9 +1,10 @@
 from django import views
 from django.urls import path
-from control_odonto.views import crear_paciente,listar_pacientes
+from control_odonto.views import listar_pacientes,crear_paciente
 
 urlpatterns = [
 
     path("lista-paciente/", listar_pacientes, name="listar_paciente"),
     # URL para la creación de un nuevo paciente
+    path("crear-paciente/", crear_paciente, name="crear_paciente"),
 ]
