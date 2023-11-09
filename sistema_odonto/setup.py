7dbdbb201dffd008e819ca67dbfd6c79f8b4b8e4
julian.arboleda <julian2320@gmail.com>
2023-11-09 18:59:09 -0500

    creacion de carpeta de setup para exportar proyecto

---
+++ sistema_odonto/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+# Llamar a la funcion setup
+setup(
+    name="Power Odontology",
+    version="1.0",
+    description="Herramientas creacion de usarios",
+    author="Julian Arboleda",
+    packages=find_packages()
+)
