a3a38b873c4ab5617f3ae0fff3f61bbbeead6c86
julian.arboleda <julian2320@gmail.com>
2023-11-15 12:12:17 -0500

    agregado views de login y url de login se modifico inicio

--- sistema_odonto/perfiles/urls.py
+++ sistema_odonto/perfiles/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from perfiles.views import registro
+from perfiles.views import registro,login_view
 
 urlpatterns = [
 path('registro/', registro, name="registro"),
+path('login/', login_view, name="login"),
 ]   
--- sistema_odonto/perfiles/views.py
+++ sistema_odonto/perfiles/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 
 from perfiles.forms import UserRegisterForm
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 def registro(request):
@@ -21,3 +24,24 @@ def registro(request):
        context={'form': formulario},
      )
 
+def login_view(request):
+   if request.method == "POST":
+       form = AuthenticationForm(request, data=request.POST)
+
+       if form.is_valid():
+           data = form.cleaned_data
+           usuario = data.get('username')
+           password = data.get('password')
+           user = authenticate(username=usuario, password=password)
+           # user puede ser un usuario o None
+           if user:
+               login(request=request, user=user)
+               url_exitosa = reverse('inicio')
+               return redirect(url_exitosa)
+   else:  # GET
+       form = AuthenticationForm()
+   return render(
+       request=request,
+       template_name='perfiles/login.html',
+       context={'form': form},
+   )
